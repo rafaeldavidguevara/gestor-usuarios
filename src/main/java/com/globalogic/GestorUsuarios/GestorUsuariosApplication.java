c0b5cae6f2491888d73ed2c4d8f73d5942b9329b
rguevara <rafaeldavidguevara@gmail.com>
2025-07-16 21:49:32 -0400

    added main project files

---
+++ src/main/java/com/globalogic/GestorUsuarios/GestorUsuariosApplication.java
@@ -0,0 +1,14 @@
+package com.globalogic.GestorUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorUsuariosApplication.class, args);
+		System.out.println("Hola Mundo!!");
+	}
+
+}
---
+++ src/test/java/com/globalogic/GestorUsuarios/GestorUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalogic.GestorUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
